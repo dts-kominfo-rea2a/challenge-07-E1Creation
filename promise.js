22f75fbc98b0479abd0be367a719b751becbeba5
Kurniawan Sudirman <kurniawanplusayahibu@gmail.com>
2022-06-09 23:18:43 +0700

    Kurniawan Sudirman: Completed

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emotion) => {
+  let sumEmotion = 0;
+  await promiseTheaterIXX().then(
+    (resultIXX) =>{
+      resultIXX.forEach((e)=>{
+          if(e.hasil == emotion) sumEmotion++;
+      });
+    }
+    );
+  await promiseTheaterVGC().then(
+    (resultVGC) => {
+      resultVGC.forEach((e)=>{
+        if(e.hasil == emotion) sumEmotion++;
+      })
+    }
+  );
+  return sumEmotion;
+};
 
 module.exports = {
   promiseOutput,
